5711c59e99290f75c244039ef8d38ea4844602ca
StefanoMRC <stefano.mrc92@gmail.com>
2022-04-20 14:43:31 +0200

    struttura

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <HeaderComp/>
+  </div>
+</template>
+
+<script>
+import "bootstrap"
+import HeaderComp from './components/HeaderComp.vue'
+export default {
+  
+  name: 'App',
+  components: {
+  HeaderComp,
+
+  }
+}
+</script>
+
+<style lang="scss">
+@import "bootstrap/dist/css/bootstrap.min.css"
+</style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="sfondo">
+    <img src="../assets/spotify-logo-png-7053.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComp',
+
+}
+</script>
+
+
+<style scoped lang="scss">
+    .sfondo{
+        background: #2e3a46;
+    }
+</style>

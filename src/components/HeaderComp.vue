0a2dbd8d114495733f116cbd98d16c9715b7cc6d
StefanoMRC <stefano.mrc92@gmail.com>
2022-04-21 09:34:55 +0200

    modifica header

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="sfondo p-3">
+  <div class="sfondo p-2">
     <img src="../assets/spotify-logo-png-7053.png" alt="">
   </div>
 </template>
@@ -14,7 +14,7 @@ export default {
 
 <style scoped lang="scss">
     .sfondo{
-        height: 8vh;
+        
         background: #2e3a46;
         img{
             height: 50px;

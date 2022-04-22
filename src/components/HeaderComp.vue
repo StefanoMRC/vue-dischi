ae8518039f25dd484e011b3d2e964b6445328937
StefanoMRC <stefano.mrc92@gmail.com>
2022-04-22 09:59:55 +0200

    modifiche

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="sfondo px-5 py-2 d-flex justify-content-between" >
     <img src="../assets/spotify-logo-png-7053.png" alt="">
-    <select class="px-1 fs-3" name="" id="" v-model="ricerca" @change.prevent="$emit('funzRicerca', ricerca)">
-      <option value="Rock">Rock</option>
-      <option value="Pop">Pop</option>
-      <option value="Jazz">Jazz</option>
-      <option value="Metal">Metal</option>
-    </select>
+    <div class="d-flex flex-column align-item-center">
+        <h5>Seleziona un genere:</h5>
+        <select class="px-1 text-center" name="" id="" v-model="ricerca" @change.prevent="$emit('funzRicerca', ricerca)">
+          <option value="">Tutti i generi</option>
+          <option value="Rock">Rock</option>
+          <option value="Pop">Pop</option>
+          <option value="Jazz">Jazz</option>
+          <option value="Metal">Metal</option>
+        </select>
+    </div>
+
   </div>
 </template>
 
@@ -33,5 +38,8 @@ export default {
         img{
             height: 50px;
         }
+    }
+    h5{
+      color: white;
     }
 </style>
